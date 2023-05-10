a98c31b9f21ff4011f23cb6b66ee7310ead20216
kozraf <kozraf@gmail.com>
2023-05-10 23:40:50 +0100

    Commeneting general_config.sh

--- general_config.sh
+++ general_config.sh
@@ -1,9 +1,9 @@
-# Configure hosts file
-echo -e "--------"
-echo -e "----Configure hosts file----"
-echo "192.168.89.141   node1.local node1" >> /etc/hosts
-echo "192.168.89.142   node2.local node2" >> /etc/hosts
-echo "192.168.89.143   node3.local node3" >> /etc/hosts
+# Configure hosts file (for other projects)
+#echo -e "--------"
+#echo -e "----Configure hosts file----"
+#echo "192.168.89.141   node1.local node1" >> /etc/hosts
+#echo "192.168.89.142   node2.local node2" >> /etc/hosts
+#echo "192.168.89.143   node3.local node3" >> /etc/hosts
 
 # Install initial software
 echo -e "--------"
